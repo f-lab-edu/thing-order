2601e7f8107e4d2142de88ba66a1dfac5226cd07
ralph <hwibaski@gmail.com>
2023-04-10 22:47:24 +0900

    feat: user id를 이용한 주문 내역 조회 임시 API 생성

--- web/src/main/java/org/example/dto/order/GetOrderByUserOutputDto.java
+++ web/src/main/java/org/example/dto/order/GetOrderByUserOutputDto.java
@@ -1,16 +1,27 @@
 package org.example.dto.order;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class GetOrderByUserOutputDto {
     private boolean ok;
     private OrderInfoResponse results;
 
     public GetOrderByUserOutputDto(boolean ok, OrderInfoResponse results) {
         this.ok = ok;
+        this.results = results;
+    }
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    public OrderInfoResponse getResults() {
+        return results;
+    }
+
+    public void setResults(OrderInfoResponse results) {
         this.results = results;
     }
 
---
+++ web/src/main/java/org/example/dto/order/OrderInfo.java
@@ -0,0 +1,14 @@
+package org.example.dto.order;
+import org.example.domain.Order;
+
+public class OrderInfo {
+    private Order order;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
