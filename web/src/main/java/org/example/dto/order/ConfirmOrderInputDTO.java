045c9e62033dc8a66670cfdb2533f6248b73008b
ralph <hwibaski@gmail.com>
2023-04-16 19:51:28 +0900

    refactor: DTO들 네이밍 명시적으로 수정

--- web/src/main/java/org/example/dto/order/ConfirmOrderInputDTO.java
+++
@@ -1,5 +0,0 @@
-package org.example.dto.order;
-
-public class ConfirmOrderInputDTO {
-    private Long orderItemId;
-}
--- web/src/main/java/org/example/dto/order/CreateOrderItemInput.java
+++
@@ -1,9 +0,0 @@
-package org.example.dto.order;
-
-
-public class CreateOrderItemInput {
-    private Long productId;
-    private Long userCouponId;
-    private Long optionId;
-    private Long orderQuantity;
-}
