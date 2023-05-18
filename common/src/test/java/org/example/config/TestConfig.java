153a0273162879d62cb5834daf350e2ef64df3b9
ralph <hwibaski@gmail.com>
2023-05-18 09:56:53 +0900

    fix: Repository 테스트 위한 설정변경 (@SpringBootApplication 어노테이션 삭제)

---
+++ common/src/test/java/org/example/config/TestConfig.java
@@ -0,0 +1,14 @@
+package org.example.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("org.example.repository")
+@EntityScan("org.example.entity")
+public class TestConfig {
+
+}
+
+
--- common/src/test/java/org/example/repository/UserRepositoryTest.java
+++ common/src/test/java/org/example/repository/UserRepositoryTest.java
@@ -1,27 +1,24 @@
 package org.example.repository;
 
+import org.example.config.TestConfig;
 import org.example.entity.User;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest(classes = {TestConfig.class, UserRepository.class}, properties = "spring.config.name=application-common-test")
+@EnableAutoConfiguration
 class UserRepositoryTest {
-
     @Autowired
     private UserRepository userRepository;
 
     @DisplayName("user의 id에 따라 user 객체를 리턴한다.")
-    @Disabled
     @Test
     void findUserWithId() {
         // given
