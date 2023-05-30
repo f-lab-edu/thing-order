5064a58230f88cb26c985091d15849721f0b4f5d
ralph <hwibaski@gmail.com>
2023-05-30 14:42:18 +0900

    feat: 사용자가 자신이 보유한 포인트보다 많은 포인트를 사용하려고 할 시 예외를 던지는 메서드 개발

---
+++ web/src/main/java/org/example/service/PointService.java
@@ -0,0 +1,30 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.entity.Coupon;
+import org.example.entity.CouponStatus;
+import org.example.entity.User;
+import org.example.exception.GraphqlException;
+import org.example.repository.CouponRepository;
+import org.example.repository.PointDetailEventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+    private PointDetailEventRepository pointDetailEventRepository;
+
+    void checkUserPoint(Long userId, Long pointAmountToUse) {
+        long sumOfTotalUserPoint = this.pointDetailEventRepository.getSumOfTotalUserPoint(userId);
+
+        if (pointAmountToUse > sumOfTotalUserPoint) {
+            HashMap<String, Object> ext = new HashMap<String, Object>();
+            ext.put("code", "NOT_ENOUGH_POINT");
+            throw new GraphqlException("You don't have enough point", ext);
+        }
+    }
+}
---
+++ web/src/test/java/org/example/service/PointServiceTest.java
@@ -0,0 +1,48 @@
+package org.example.service;
+
+import org.example.exception.GraphqlException;
+import org.example.repository.PointDetailEventRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class PointServiceTest {
+    @Mock
+    private PointDetailEventRepository pointDetailEventRepository;
+    @InjectMocks
+    private PointService pointService;
+
+    @DisplayName("사용자가 자신이 보유한 포인트보다 많은 포인트를 사용하려고 할 시 예외를 던진다.")
+    @Test
+    void checkUserCouponStatus1() {
+        // given
+        long userIdHasNotEnoughPoint = 1L;
+        Long pointHoldsByUser = 1000L;
+        Long pointToUserWantToUse = 2000L;
+        given(pointDetailEventRepository.getSumOfTotalUserPoint(userIdHasNotEnoughPoint)).willReturn(pointHoldsByUser);
+
+        // when
+        // then
+        assertThatThrownBy(() -> pointService.checkUserPoint (userIdHasNotEnoughPoint, pointToUserWantToUse))
+                .isInstanceOf(GraphqlException.class)
+                .hasMessage("You don't have enough point")
+                .satisfies(exception -> {
+                    if (exception instanceof GraphqlException) {
+                        GraphqlException graphqlException = (GraphqlException) exception;
+                        assertThat(graphqlException.getExtensions()).isNotNull();
+                        assertThat(graphqlException.getExtensions().get("code")).isEqualTo(
+                                "NOT_ENOUGH_POINT");
+                    }
+                });
+    }
+}
