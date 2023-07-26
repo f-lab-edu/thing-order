c439bf3feff7a32d63cd303b8bae9eb8a8395717
ralph <hwibaski@gmail.com>
2023-07-26 13:29:02 +0900

    fix: 구현되어 있지 않은 api 삭제

--- web/src/main/java/org/example/resolver/OrderResolver.java
+++ web/src/main/java/org/example/resolver/OrderResolver.java
@@ -1,13 +1,9 @@
 package org.example.resolver;
 
-import org.example.dto.order.ConfirmMemberOrderResponseDto;
-import org.example.dto.order.ConfirmOrderRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.example.dto.order.CreateMemberOrderRequestDto;
 import org.example.dto.order.CreateMemberOrderResponseDto;
-import org.example.dto.order.GetMemberOrderByUserRequestDto;
-import org.example.dto.order.GetMemberOrderByUserResponseDto;
 import org.example.dto.order.JwtPayload;
-import org.example.dto.order.OrderInfoResponse;
 import org.example.entity.Order;
 import org.example.entity.User;
 import org.example.exception.GraphqlException;
@@ -17,10 +13,7 @@ import org.example.service.UserService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.ContextValue;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,36 +25,24 @@ public class OrderResolver {
 
     @MutationMapping
     public CreateMemberOrderResponseDto createMemberOrderV3(
-        @Argument("input") CreateMemberOrderRequestDto requestDto,
-        @ContextValue(name = "x-jwt") String jwtAccessToken) throws Exception {
+            @Argument("input") CreateMemberOrderRequestDto requestDto,
+            @ContextValue(name = "x-jwt") String jwtAccessToken) throws Exception {
         if (!this.jwtService.verifyToken(jwtAccessToken)) {
             throw new GraphqlException("403 FORBIDDEN");
         }
 
         JwtPayload jwtPayload = this.jwtService.getJwtPayload(jwtAccessToken);
         User user = this.userService.findUserById(jwtPayload.getId())
-            .orElseThrow(() -> new Exception("user not found"));
+                .orElseThrow(() -> new Exception("user not found"));
 
         Order newOrder = this.orderService.createMemberOrder(requestDto.getPaymentMethod(), user,
-            requestDto.getItems(), requestDto.getPointDiscountPrice(), requestDto.getDeliveryId(),
-            requestDto.getDeliveryMessage());
+                requestDto.getItems(), requestDto.getPointDiscountPrice(), requestDto.getDeliveryId(),
+                requestDto.getDeliveryMessage());
 
         if (newOrder.isZeroPaid()) {
             return new CreateMemberOrderResponseDto(true, newOrder, true);
         }
 
         return new CreateMemberOrderResponseDto(true, newOrder, false);
-    }
-
-    @QueryMapping
-    public GetMemberOrderByUserResponseDto order(
-        @Argument("input") GetMemberOrderByUserRequestDto getMemberOrderByUserRequestDto) {
-        return new GetMemberOrderByUserResponseDto(true, new OrderInfoResponse());
-    }
-
-    @MutationMapping
-    public ConfirmMemberOrderResponseDto confirmMemberOrder(
-        ConfirmOrderRequestDto confirmOrderRequestDTO) {
-        return new ConfirmMemberOrderResponseDto(true, true);
     }
 }
